3a4d24b1cd2cd97d8fd49b59bcff39df5de8923f
Tracey <radcliffe.tracey@gmail.com>
2016-08-23 11:26:18 -0700

    added diff images

--- lab23-traceyr/app/data/images.js
+++ lab23-traceyr/app/data/images.js
@@ -3,22 +3,22 @@
 module.exports = [
   {
     id: 1,
-    url: 'https://www.flickr.com/photos/luckyno3/29008125026/in/pool-welshcorgis/',
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdvImmIxGEkp4Cs9zmXFWxQUbpjkXyAoy8h02p07cMr3sMhsrH',
     description: 'Cute Corgi 1'
   },
   {
     id: 2,
-    url: 'https://www.flickr.com/photos/eschweik/27945680922/in/pool-welshcorgis/',
+    url: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSIY7ZUqKIzW5ISQ4EiAYCNgIcVnaoeYj5BRIEPtaHAVPfjFGBy',
     description: 'Cute Corgi 2'
   },
   {
     id: 3,
-    url: 'https://www.flickr.com/groups/welshcorgis/pool/',
+    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD2JpdXRjUSq2FPYvstqecIvrOHOmKVGEmUmqy2362Nrr9FbV-',
     description: 'Cute Corgi 3'
   },
   {
     id: 4,
-    url: 'https://www.flickr.com/photos/les_sens_ordinaires/7703125592/',
+    url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRa4N5QWH3ZxeHjZYt3RB7iWRj1GXRI_LQzTQf9CuspJx7Cfs_8UA',
     description: 'Cute Corgi 4'
   }
 ];
