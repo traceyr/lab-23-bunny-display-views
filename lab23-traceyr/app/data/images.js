8cba9d043dc2f13e404bdb0df36e4f944b0b8e5f
Tracey <radcliffe.tracey@gmail.com>
2016-08-23 10:15:13 -0700

    added pics

---
+++ lab23-traceyr/app/data/images.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = [
+  {
+    id: 1,
+    url: 'https://www.flickr.com/photos/luckyno3/29008125026/in/pool-welshcorgis/',
+    description: 'Cute Corgi 1'
+  },
+  {
+    id: 2,
+    url: 'https://www.flickr.com/photos/eschweik/27945680922/in/pool-welshcorgis/'
+  },
+  {
+    id: 3,
+    url: 'https://www.flickr.com/groups/welshcorgis/pool/',
+    description: 'Cute Corgi 3'
+  },
+  {
+    id: 4,
+    url: 'https://www.flickr.com/photos/les_sens_ordinaires/7703125592/',
+    description: 'Cute Corgi 4'
+  }
+];
