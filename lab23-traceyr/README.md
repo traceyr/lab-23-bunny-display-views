7dd6667d0264318fb6ae2ed2a967a5fcaee5b6b4
Tracey <radcliffe.tracey@gmail.com>
2016-08-23 12:15:03 -0700

    finished testing

--- lab23-traceyr/README.md
+++ lab23-traceyr/README.md
@@ -1,1 +1,15 @@
 #Lab 23
+
+-Git Clone the lab and run: npm install
+-In one terminal run: npm run watch
+-In the browser open: localhost 8080
+
+-To get the backend working
+--clone this directory down(not in the lab22 directory): https://github.com/codefellows-seattle-javascript-401n1/noteapp-backend
+--cd into noteapp-backend
+-in a tab type: npm install
+--in another tab type: mongod --dbpath ./db
+--in another tab type: npm run watch
+
+-Click on different parts of the website to see cute corgis
+-To run tests, in a second terminal run: karma start
--- lab23-traceyr/test/controller-test.js
+++ lab23-traceyr/test/controller-test.js
@@ -4,15 +4,25 @@
 
 describe('testing controllers used in routing', function() {
   beforeEach(() => {
-    angular.mock.module('demoApp');
+    angular.mock.module('lab23');
     angular.mock.inject(($controller) => {
       this.homeController = new $controller('HomeController');
+      this.fullsizeController = new $controller('FullsizeController');
     });
   });
 
-  it('home controller should create a list of messages', () => {
-    expect(this.homeController.messages.length > 0).toEqual(true);
-    expect(this.homeController.messages.length).toEqual(3);
+  it('home controller should create a list of images', () => {
+    expect(this.homeController.images.length > 0).toEqual(true);
+    expect(this.homeController.images.length).toEqual(4);
   });
 
+  it('fullsize controller should create a list of images', () => {
+    expect(this.fullsizeController.images.length > 0).toEqual(true);
+    expect(this.fullsizeController.images.length).toEqual(4);
+  });
+
+  it('fullsize controller should test for a valid id', () => {
+    expect(this.fullsizeController.isValidId('ted')).toEqual(false);
+    expect(this.fullsizeController.isValidId(3)).toEqual(true);
+  });
 });
